35be82e6bc7f1357c98f767e27583b71242c47f8
Enoch Tam <et.enoch@gmail.com>
2015-05-16 19:31:43 -0400

    + Action Bar style + Menu search icon

--- app/src/main/java/ca/goldenwords/gwandroid/MainActivity.java
+++ app/src/main/java/ca/goldenwords/gwandroid/MainActivity.java
@@ -1,23 +1,23 @@
 package ca.goldenwords.gwandroid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_search) {
             return true;
         }
 
