873673f628ed3c1fc022e8e4caaa24b41678d19a
Enoch Tam <et.enoch@gmail.com>
2015-06-21 16:02:26 -0400

    + build.gradle fixes & additions + fixed build breaking

--- app/src/main/java/ca/goldenwords/gwandroid/utils/RevisionDateComparator.java
+++ app/src/main/java/ca/goldenwords/gwandroid/utils/RevisionDateComparator.java
@@ -9,7 +9,7 @@ public class RevisionDateComparator implements Comparator<Node> {
 
     @Override public int compare(Node n1, Node n2) {
         int r = -1;
-        if (n1.revision<n2.revision) r = 1;
+        if (n1.revision_timestamp<n2.revision_timestamp) r = 1;
         return r;
     }
 }
